c39ab6f677807ad7e5dc0e0c94feeaa444f7465c
Michael Martin <3277009+flrgh@users.noreply.github.com>
2023-04-10 17:30:06 -0700

    tests(wasm): add foreign relationship API tests

--- spec/fixtures/blueprints.lua
+++ spec/fixtures/blueprints.lua
@@ -401,8 +401,11 @@ function _M.new(db)
     }
   end)
 
+  local filter_chains_seq = new_sequence("filter-chains-%d")
   res.filter_chains = new_blueprint(db.filter_chains, function()
-    return {}
+    return {
+      name = filter_chains_seq:next(),
+    }
   end)
 
   return res
